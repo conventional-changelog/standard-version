c8c51443e6dacc6ced2c6484d8e407cf66302235
Benjamin Coe <ben@npmjs.com>
2018-11-01 10:39:21 -0700

    fix: CLI should fail on old Node.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-var standardVersion = require('../index')
-var cmdParser = require('../command')
 
 /* istanbul ignore if */
-if (process.version.match(/v(\d+)\./)[1] < 4) {
-  console.error('standard-version: Node v4 or greater is required. `standard-version` did not run.')
+if (process.version.match(/v(\d+)\./)[1] < 6) {
+  console.error('standard-version: Node v6 or greater is required. `standard-version` did not run.')
 } else {
+  const standardVersion = require('../index')
+  const cmdParser = require('../command')
   standardVersion(cmdParser.argv)
     .catch(() => {
       process.exit(1)
