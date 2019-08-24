376ca60bd489f191e2bfacb9d3eba02be1ca5818
Joe Bottigliero <joe.bottigliero@gmail.com>
2019-08-24 08:26:56 -0500

    fix: packageFiles
    - Ensure `packageFile` parsing supports custo updaters.
    - Abstracts the updater resolution code for bumpFiles and packageFiles

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const latestSemverTag = require('./lib/latest-semver-tag')
 const path = require('path')
 const printError = require('./lib/print-error')
 const tag = require('./lib/lifecycles/tag')
+const { resolveUpdaterObjectFromArgument } = require('./lib/updaters')
 
 module.exports = function standardVersion (argv) {
   let defaults = require('./defaults')
@@ -27,12 +28,16 @@ module.exports = function standardVersion (argv) {
 
   const args = Object.assign({}, defaults, argv)
   let pkg
-  args.packageFiles.forEach((filename) => {
+  args.packageFiles.forEach((packageFile) => {
     if (pkg) return
-    const pkgPath = path.resolve(process.cwd(), filename)
+    const updater = resolveUpdaterObjectFromArgument(packageFile)
+    const pkgPath = path.resolve(process.cwd(), updater.filename)
     try {
-      const data = fs.readFileSync(pkgPath, 'utf8')
-      pkg = JSON.parse(data)
+      const contents = fs.readFileSync(pkgPath, 'utf8')
+      pkg = {
+        version: updater.updater.readVersion(contents),
+        private: typeof updater.updater.isPrivate === 'function' ? updater.updater.isPrivate(contents) : false
+      }
     } catch (err) {}
   })
   let newVersion
